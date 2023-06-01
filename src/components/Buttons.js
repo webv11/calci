641d51c6b35b3513a2b251bfaec64c5ae42a8123
vaibhavi11thakur <vaibhavi.thakur@preciousinfosystem.com>
2023-06-01 14:31:15 +0530

    hii

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,19 @@
 import './styles/buttons.css';
 
 const rowStyle = {
-    display: "flex", 
+    display: "flex",
 }
 
-let Buttons = ({data, onClick}) => {
+let Buttons = ({ data, onClick }) => {
     return (
         <div className="container">
             {data && data.map((item, index) => {
-                return <div style={rowStyle} key={`btnRow_${index}`}> 
-                    {item && item.map((label, lindex) =>  label !== "" ?  <button onClick={onClick} key={`btnitem_${lindex}`} className="button">{label}</button > : <span key={`btnitem_${lindex}`}></span>)}
+                return <div style={rowStyle} key={`btnRow_${index}`}>
+                    {item && item.map((label, lindex) => label !== "" ?
+                        <button onClick={onClick} key={`btnitem_${lindex}`} className="button">{label}</button > :
+                        <span key={`btnitem_${lindex}`}></span>)}
                 </div>
+
             })}
         </div>
     );
